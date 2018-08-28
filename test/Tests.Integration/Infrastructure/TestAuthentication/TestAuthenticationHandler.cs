0f68218e4445e9a366162ada1554053b0c6bd930
Gus Gray <agray1219@yahoo.com>
2018-08-27 22:10:21 -0700

    Some stuff for a test auth handler.

---
+++ test/Tests.Integration/Infrastructure/TestAuthentication/TestAuthenticationHandler.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using System;
+using System.Collections.Generic;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tests.Integration.Infrastructure.TestAuthentication
+{
+    public class TestAuthenticationHandler : AuthenticationHandler<TestAuthenticationOptions>
+    {
+        protected override Task<AuthenticateResult> HandleAuthenticateAsync()
+        {
+            var authenticationTicket = new AuthenticationTicket(
+                new ClaimsPrincipal(Options.Identity),
+                new AuthenticationProperties());
+
+            return Task.FromResult(AuthenticateResult.Success(authenticationTicket));
+        }
+    }
+}
---
+++ test/Tests.Integration/Infrastructure/TestAuthentication/TestAuthenticationOptions.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using System;
+using System.Collections.Generic;
+using System.Security.Claims;
+using System.Text;
+
+namespace Tests.Integration.Infrastructure.TestAuthentication
+{
+    public class TestAuthenticationOptions : AuthenticationSchemeOptions
+    {
+        public TestAuthenticationOptions()
+        {
+        }
+        public virtual ClaimsIdentity Identity { get; } = new ClaimsIdentity(new Claim[]
+        {
+            new Claim("", "")
+        }, "testAuthentication");
+    };
+}
